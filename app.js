bb824fbbf5eaa44b40a4fb2892ef36e6ca34101d
RubelHasan <rubelhasan306@gmail.com>
2021-08-25 02:31:45 +0600

    Added event listener

--- app.js
+++ app.js
@@ -4,29 +4,44 @@ const button = document.querySelector('button');
 const input = document.querySelector('#input-search');
 const temp = document.querySelector('.tempValue');
 const description = document.querySelector('.description');
-const humidity = document.querySelector('.humidity');
-const wind = document.querySelector('.wind');
-
-
-const API_KEY = 'a5f6643d32df368b4c55a92fe44c5db0';
-let API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=' + input.value +'&appid=a5f6643d32df368b4c55a92fe44c5db0';
-    fetch(API_URL)
-    .then(res => res.json())
-    .then(data => showingData(data))
-    .catch(error => console.log("data loading fail"));
-
+const humidity = document.querySelector('.humidity span');
+const wind = document.querySelector('.wind span');
+const icon = document.querySelector('.icon');
 
 
 
 button.addEventListener('click', (e)=>{
     e.preventDefault();
 
+    
+    const API_KEY = 'a5f6643d32df368b4c55a92fe44c5db0';
+    let API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=' + input.value +'&appid=a5f6643d32df368b4c55a92fe44c5db0';
+        fetch(API_URL)
+        .then(res => res.json())
+        .then(data => showingData(data))
+        .catch(error => console.log("data loading fail"));
+
+
+        function showingData(data){
+            //data
+            const {humidity, temp} = data.main;
+            const {name} = data;
+            const {speed} = data.wind;
+            const {description, icon} = data.weather[0];
+            //adding data
+            cityName.innerText = name;
+            description.innerText = data.weather[0].description;
+            humidity.innerText = data.main.humidity;
+            wind.innerText = data.wind.speed;
+            icon.innerText = data.weather[0].icon;
+            temp.innerText = data.main.temp;
+
+
+            console.log(data);
+        }
+
+
+
+
 })
 
-
-function showingData(data){
-    //data
-    const {humidity, temp} = data.main;
-    const {name} = data.name;
-    console.log(data.name);
-}
